0ad8496da773f660325441294d10d6a90a08612c
Pierre Basck <pierre.basck@gmail.com>
2022-10-06 11:12:35 +0200

    Game

--- index.js
+++ index.js
@@ -1,2 +1,50 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(namee, strength, dexterity, life) {
+    this.namee = namee;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(attacked) {
+    let damageDone =
+      Math.max(1, Math.floor(Math.random() * this.strength * 2)) - attacked.dexterity;
+    if (damageDone < 0) {
+      console.log(`${this.namee} miss! 🛡️`);
+    }
+    else {
+      console.log(
+        `${this.namee} ⚔️ ${attacked.namee} for ${damageDone} HP`
+      );
+      return (attacked.life = attacked.life - damageDone);
+    }
+  }
+
+}
+
+const Heracles = new Fighter("👨Heracles", 20, 6);
+const NemeanLion = new Fighter("😺Nemean Lion", 11, 13);
+
+
+function deathMatch(fighter1, fighter2) {
+    let count = 0;
+    while(fighter1.life > 0 && fighter2.life > 0) {
+    console.log(`🎲 ROUND ${count}`);
+    fighter1.fight(fighter2);
+    console.log(`${fighter2.namee} ❤️: ${fighter2.life}`);
+    if(fighter2.life <= 0){
+        console.log(`${fighter2.namee} : 💀, ✌️${fighter1.namee} wins!✌️`)
+        break;};
+    fighter2.fight(fighter1);
+    console.log(`${fighter1.namee} ❤️: ${fighter1.life}`);
+    if(fighter1.life <= 0){
+        console.log(`${fighter1.namee} : 💀, ✌️${fighter2.namee} wins!✌️`)
+        break;};
+    count++;
+}}
+
+deathMatch(Heracles, NemeanLion);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,11 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor(namee, strength, dexterity, life){
+    this.namee = namee;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+}}
